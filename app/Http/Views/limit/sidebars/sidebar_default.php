2ba0cce0a116589a3263227da1cc2eadee8b3bf0
Dennis Agulo <dennisagulo@gmail.com>
2017-06-29 13:16:28 +0800

    update sidebar

--- app/Http/Views/limit/sidebars/sidebar_default.php
+++ app/Http/Views/limit/sidebars/sidebar_default.php
@@ -4,10 +4,10 @@
             <li><a href="<?php echo Url('component/leads') ?>"><i class="icon-people"></i> <span> Leads </span></a></li>
             <li><a href="<?php echo Url('component/properties') ?>"><i class="icon-home"></i> <span> Properties </span></a></li>
             <li>
-                <a href="javascript:"><i class="icon-home"></i> <span> Marketing </span></a>
+                <a href="javascript:"><i class="icon-file-empty2"></i> <span> Marketing </span></a>
                 <ul>
-                    <li><a href="<?php echo Url('component/marketing') ?>"><i class="icon-home"></i> <span> Marketing Campaigns </span></a></li>
-                    <li><a href="<?php echo Url('component/marketing/postcards') ?>"><i class="icon-home"></i> <span> Postcards </span></a></li>
+                    <li><a href="<?php echo Url('component/marketing') ?>"><i class="icon-stack"></i> <span> Marketing Campaigns </span></a></li>
+                    <li><a href="<?php echo Url('component/marketing/postcards') ?>"><i class="icon-image5"></i> <span> Postcards </span></a></li>
                 </ul>
             </li>
             <li><a href="<?php echo Url('component/settings') ?>"><i class="icon-gear"></i> <span> Settings </span></a></li>
