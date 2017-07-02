68f089ce6e8ba0cb5a71773b6818e8f841955162
Dennis Agulo <dennisagulo@gmail.com>
2017-07-02 20:43:54 +0800

    Fix 'From' address

--- app/Console/Commands/SendPostcards.php
+++ app/Console/Commands/SendPostcards.php
@@ -136,17 +136,22 @@ class SendPostcards extends Command
     private function accountAddress( $user_map )
     {
         if( $user_map->broker_userid == $user_map->userid ){
-            $name = $user_map->br_first_name.' '.$user_map->br_last_name;
-            $address = '';
-            $city = '';
-            $state = '';
-            $postal_code = '';
+
+
+            $name       = $user_map->br_first_name.' '.$user_map->br_last_name;
+            $address    = $user_map->br_address;
+            $city       = $user_map->br_city;
+            $s  = States::where( 'id' , $user_map->stateid )->first();
+            $state      = $s ? $s->stateid : 'CA';
+            $postal_code = $user_map->zip;
         }else{
-            $name = ' ';
-            $address = '';
-            $city = '';
-            $state = '';
-            $postal_code = '';
+
+            $name       = $user_map->contact_firstname.' '.$user_map->contact_lastname;
+            $address    = $user_map->contact_street;
+            $city       = $user_map->contact_city;
+            $s  = States::where( 'id' , $user_map->contact_stateid )->first();
+            $state          = $s ? $s->stateid : 'CA';
+            $postal_code    = $user_map->contact_zipcode;
         }
 
         return [
